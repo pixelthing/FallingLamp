bee57622e71ed0f21ed7272265ac3aff54f743f1
Craig Morey <craigmorey@gmail.com>
2017-09-28 16:12:10 +0200

    tweaks to give new look and feel

--- js/ropeCore.js
+++ js/ropeCore.js
@@ -3,8 +3,8 @@ var ropeDemo = {
     data: {
         fps: +60,
         intervalId: null,
-        gravity: +20.5,
-        pixelsPerMeter: +150
+        gravity: +20,
+        pixelsPerMeter: +300
     },
 
     context: {
@@ -15,7 +15,7 @@ var ropeDemo = {
         mouse: { x: +0, y: +0 },
         isGrabbing: false,
         img0: null,
-        img1:null,
+        img1: null,
         stopped: false
     },
 
@@ -25,10 +25,13 @@ var ropeDemo = {
 
     Step: function () {
         ropeDemo.ThinkOverride();
-        if (Math.abs(ropeDemo.rope.coeff)>0.0001 || isNaN(ropeDemo.rope.coeff)){
+        if (
+            Math.abs(ropeDemo.rope.coeff) > 0.00001 || 
+            isNaN(ropeDemo.rope.coeff)
+        ){
             ropeDemo.DrawOverride();
 
-        }else{
+        } else {
             ropeDemo.Stop();
             ropeDemo.waitLight();
         }
@@ -38,7 +41,6 @@ var ropeDemo = {
 
     Start: function () {
         ropeDemo.StartOverride();
-
         ropeDemo.data.intervalId = setInterval(ropeDemo.Step, +1000 / ropeDemo.data.fps);
     },
 
@@ -55,7 +57,7 @@ var ropeDemo = {
         ropeDemo.context.canvas.height = window.innerHeight;
         ropeDemo.context.size.w = window.innerWidth;
         ropeDemo.context.size.h = window.innerHeight;
-                        ropeDemo.context.drawingContext = ropeDemo.context.canvas.getContext("2d");
+        ropeDemo.context.drawingContext = ropeDemo.context.canvas.getContext("2d");
 
         console.log(ropeDemo.context.size.w);        
         console.log(ropeDemo.context.size.h);
@@ -63,47 +65,43 @@ var ropeDemo = {
         ropeDemo.context.center.x = ropeDemo.context.size.w * 0.5;
         ropeDemo.context.center.y = ropeDemo.context.size.h * +0;
 
-        //future grabbing implementation:
-        // ropeDemo.context.canvas.onmousemove = function (e) {
-        //     if (e.offsetX) {
-        //         ropeDemo.context.mouse.x = e.offsetX;
-        //         ropeDemo.context.mouse.y = e.offsetY;
-        //     } else if (e.layerX) {
-        //         ropeDemo.context.mouse.x = e.layerX;
-        //         ropeDemo.context.mouse.y = e.layerY;
-        //     }
-        // };
+        // grabbing
+        //*
+        ropeDemo.context.canvas.onmousemove = function (e) {
+            if (e.offsetX) {
+                ropeDemo.context.mouse.x = e.offsetX;
+                ropeDemo.context.mouse.y = e.offsetY;
+            } else if (e.layerX) {
+                ropeDemo.context.mouse.x = e.layerX;
+                ropeDemo.context.mouse.y = e.layerY;
+            }
+        };
+        ropeDemo.context.canvas.onmousedown = function (e) {
+            ropeDemo.context.isGrabbing = true;
+        };
+        ropeDemo.context.canvas.onmouseup = function (e) {
+            ropeDemo.context.isGrabbing = false;
+        };
+        ropeDemo.context.canvas.onmouseout = function (e) {
+            ropeDemo.context.isGrabbing = false;
+        };
+        //*/
 
-        // ropeDemo.context.canvas.onmousedown = function (e) {
-        //     ropeDemo.context.isGrabbing = true;
-        // };
 
-        // ropeDemo.context.canvas.onmouseup = function (e) {
-        //     ropeDemo.context.isGrabbing = false;
-        // };
-
-        // ropeDemo.context.canvas.onmouseout = function (e) {
-        //     ropeDemo.context.isGrabbing = false;
-        // };
-
+        // load the bulb images
         ropeDemo.context.img0 = new Image();
         ropeDemo.context.img1 = new Image();
         
+        //IMPORTANT: Wait for the picture to be loaded!
+        ropeDemo.context.img0.onload = function(){
+            console.log('img loaded')
+            // Start application
+            ropeDemo.Start();
+        };
 
-            //IMPORTANT: Wait for the picture to be loaded!
-            ropeDemo.context.img0.onload = function(){
-
-                  // Start application
-             ropeDemo.Start();
-            };
-
-            //yes, the src goes after
-            ropeDemo.context.img0.src = 'lamp0.png';
-            ropeDemo.context.img1.src = 'lamp1.png';
-
-
-       
-        
+        //yes, the src goes after
+        ropeDemo.context.img0.src = 'bulb-off.png';
+        ropeDemo.context.img1.src = 'bulb-on.png';
 
     },
     Light: function () { throw "Not implemented"; },
--- js/ropeImpl.js
+++ js/ropeImpl.js
@@ -1,18 +1,27 @@
 ropeDemo.rope = {
 	items: [],
 	nbItems: 20,
-	length: +50,
-	relaxationIterations: +10,
+	length: +400,
+	width: 8,
+	relaxationIterations: +5,
 	coeff:+1,
 	state: false,
 	neon: null,
-	oldCoeff: null
+	oldCoeff: null,
+	color1: '#ccc',
+	color2: '#999'
+};
+ropeDemo.bulb = {
+	width: 121,
+	height: 203,
+	offsetX: -59,
+	offsetY: -20,
+	maxRotation: 0.5
 };
 
+// one cycle - draw to screen
+
 ropeDemo.DrawOverride = function () {
-
-  
-
 
 	// Draw segments
 
@@ -20,75 +29,89 @@ ropeDemo.DrawOverride = function () {
 	
 	ropeDemo.context.drawingContext.beginPath();
 
+	// loop through the items to draw
 	for (var index in ropeDemo.rope.items) {
+
+		// this item
 		var item = ropeDemo.rope.items[index];
 
-
+		// first segment
 		if (index == 0) {
 			ropeDemo.context.drawingContext.moveTo(item.x + ropeDemo.context.center.x, item.y + ropeDemo.context.center.y);
-			
+		
+		// draw the rope
 		} else if (item.isRope == true){
-				ropeDemo.context.drawingContext.lineTo(item.x + ropeDemo.context.center.x, item.y + ropeDemo.context.center.y);
-				
-			
-				
+
+			ropeDemo.context.drawingContext.lineTo(item.x + ropeDemo.context.center.x, item.y + ropeDemo.context.center.y);
 			   
-		}else if (item.isRope == false){
+		// draw the bulb
+		} else if (item.isRope == false){
 
-				var item0 = ropeDemo.rope.items[index-18];
-				ropeDemo.rope.coeff = ((item.x + ropeDemo.context.center.x)- (item0.x + ropeDemo.context.center.x))/((item.y + ropeDemo.context.center.y)-(item0.y + ropeDemo.context.center.y));
+			var item0 = ropeDemo.rope.items[index - (ropeDemo.rope.nbItems - 1)];
+			ropeDemo.rope.coeff = (
+					(item.x + ropeDemo.context.center.x) - 
+					(item0.x + ropeDemo.context.center.x)
+				)/(
+					(item.y + ropeDemo.context.center.y) -
+					(item0.y + ropeDemo.context.center.y)
+				);
 
-				//future grabbing implementation:
-				// if(ropeDemo.rope.coeff > 1.5){
-
-				// 	ropeDemo.rope.coeff = 1.5;
-				// }else if(ropeDemo.rope.coeff < -1.5){
-				// 	ropeDemo.rope.coeff = -1.5;
-				// }
-				// if(Math.abs(ropeDemo.rope.oldCoeff-ropeDemo.rope.coeff) > 0.5 ){
-				// 	delta = -0.5;
-				// 	if(ropeDemo.rope.coeff<0){
-				// 		delta = 0.5;
-				// 	}
-				// 	ropeDemo.rope.coeff = ropeDemo.rope.oldCoeff + delta; 
-				// }
-				// ropeDemo.rope.oldCoeff = ropeDemo.rope.coeff;
-
-				console.log(ropeDemo.rope.coeff );
-				ropeDemo.context.drawingContext.save();
-				ropeDemo.context.drawingContext.translate((item.x + ropeDemo.context.center.x), (item.y + ropeDemo.context.center.y));
-				ropeDemo.context.drawingContext.rotate(ropeDemo.rope.coeff);
-				
-				//the light
-				
+			//future grabbing implementation:
+			//if(ropeDemo.rope.coeff > 1.5) {
+			//	ropeDemo.rope.coeff = 1.5;
+			//} else if(ropeDemo.rope.coeff < -1.5) {
+			//	ropeDemo.rope.coeff = -1.5;
+			//}
+			//if(Math.abs(ropeDemo.rope.oldCoeff-ropeDemo.rope.coeff) > 0.5 ) {
+			//	delta = -0.5;
+			//	if(ropeDemo.rope.coeff<0) {
+			//		delta = 0.5;
+			//	}
+			//	ropeDemo.rope.coeff = ropeDemo.rope.oldCoeff + delta; 
+			//}
+			//ropeDemo.rope.oldCoeff = ropeDemo.rope.coeff;
+			//console.log(ropeDemo.rope.coeff );
+			ropeDemo.context.drawingContext.save();
+			ropeDemo.context.drawingContext.translate((item.x + ropeDemo.context.center.x), (item.y + ropeDemo.context.center.y));
+			// restrict the rotation
+			var rotation = ropeDemo.rope.coeff;
+			if (rotation > ropeDemo.bulb.maxRotation) {
+				rotation = ropeDemo.bulb.maxRotation;
+			} else if (rotation < -ropeDemo.bulb.maxRotation) {
+				rotation = -ropeDemo.bulb.maxRotation;
+			}
+			ropeDemo.context.drawingContext.rotate(-rotation);
 			
-				ropeDemo.context.drawingContext.drawImage(ropeDemo.context.img0,-150,-10); 
-				
-
-				ropeDemo.context.drawingContext.restore();
+			//the light
+			ropeDemo.context.drawingContext.drawImage(ropeDemo.context.img0,ropeDemo.bulb.offsetX,ropeDemo.bulb.offsetY,ropeDemo.bulb.width,ropeDemo.bulb.height); 
+			
+			ropeDemo.context.drawingContext.restore();
 				
 		}
 	}
-	var grad= ropeDemo.context.drawingContext.createLinearGradient((ropeDemo.context.size.w * 0.5)-10, 0, (ropeDemo.context.size.w * 0.5)+20,0);
-	grad.addColorStop(0, "#666666");
-	grad.addColorStop(1, "#111111");
+	var center = ropeDemo.context.size.w * 0.5 + 20;
+	var ropeRadius = ropeDemo.rope.width/2;
+	var grad = ropeDemo.context.drawingContext.createLinearGradient(center - ropeRadius, 0, center + ropeRadius, 0);
+	grad.addColorStop(0, ropeDemo.rope.color1);
+	grad.addColorStop(1, ropeDemo.rope.color2);
 
 	ropeDemo.context.drawingContext.strokeStyle = grad;
-	ropeDemo.context.drawingContext.lineWidth = 7;
+	ropeDemo.context.drawingContext.lineWidth = ropeDemo.rope.width;
 	ropeDemo.context.drawingContext.stroke();
 	ropeDemo.context.drawingContext.closePath();
 
-
-
 };
 
+// one cycle - calculations
+
 ropeDemo.ThinkOverride = function () {
+	
 	var itemLength = ropeDemo.rope.length / ropeDemo.rope.nbItems;
 	var ellapsedTime = +1 / ropeDemo.data.fps;
 
 	 if (ropeDemo.context.isGrabbing) {
-        ropeDemo.rope.items[19].x = ropeDemo.context.mouse.x - ropeDemo.context.center.x;
-        ropeDemo.rope.items[19].y = ropeDemo.context.mouse.y - ropeDemo.context.center.y;
+        ropeDemo.rope.items[(ropeDemo.rope.nbItems-1)].x = ropeDemo.context.mouse.x - ropeDemo.context.center.x;
+        ropeDemo.rope.items[(ropeDemo.rope.nbItems-1)].y = ropeDemo.context.mouse.y - ropeDemo.context.center.y;
     }
    
 	// Apply verlet integration
@@ -97,7 +120,6 @@ ropeDemo.ThinkOverride = function () {
 
 		var old_x = item.x;
 		var old_y = item.y;
-
 
 		if (!item.isPinned ) {
 			physic.ApplyUnitaryVerletIntegration(item, ellapsedTime, ropeDemo.data.gravity, ropeDemo.data.pixelsPerMeter);
@@ -134,116 +156,102 @@ ropeDemo.ThinkOverride = function () {
 	}
 };
 ropeDemo.playLight = function(){
-				ropeDemo.song = document.getElementById('neonSound');
-
-				ropeDemo.song.currentTime = 0;
-				ropeDemo.song.play();
 
 }
 ropeDemo.playCreak = function(){
-				ropeDemo.creak = document.getElementById('creakSound');
-
-				ropeDemo.creak.play();
-
 }
 
 ropeDemo.waitLight = function(){
-				// document.body.style.background="#ddd";
-				ropeDemo.playLight();
-				setTimeout(function(){ropeDemo.Light(); }, 400);
-				setTimeout(function(){ropeDemo.noLight()},450);
-
-				setTimeout(function(){ropeDemo.Light()}, 600);
-				setTimeout(function(){ropeDemo.noLight()}, 700);
-
-				setTimeout(function(){ropeDemo.Light()}, 1100);
-			$('.dustCloser').css({'opacity':'1'}).addClass('dustCloserAnimated');
-			$('.dustFar').css({'opacity':'1'}).addClass('dustFarAnimated');
-
+	ropeDemo.playLight();
+	ropeDemo.Light();
+//	setTimeout(function(){ropeDemo.Light(); }, 400);
+//	setTimeout(function(){ropeDemo.noLight()}, 450);
+//
+//	setTimeout(function(){ropeDemo.Light()},   600);
+//	setTimeout(function(){ropeDemo.noLight()}, 700);
+//
+//	setTimeout(function(){ropeDemo.Light()},   1100);
 }
+
 ropeDemo.Light = function(){
-				$('.central').css({'opacity':'1'});
-				ropeDemo.data.state = true;
-				ropeDemo.context.drawingContext.beginPath();
-				 
-           		
 
-				var item = ropeDemo.rope.items.slice(-1)[0];
-				var item0 = ropeDemo.rope.items.slice(-9)[0];
-				ropeDemo.rope.coeff = ((item.x + ropeDemo.context.center.x)- (item0.x + ropeDemo.context.center.x))/((item.y + ropeDemo.context.center.y)-(item0.y + ropeDemo.context.center.y));
-				ropeDemo.context.drawingContext.save();
-				ropeDemo.context.drawingContext.translate((item.x + ropeDemo.context.center.x), (item.y + ropeDemo.context.center.y));
-				ropeDemo.context.drawingContext.rotate(ropeDemo.rope.coeff);
-				
-				//the light
-				
-				ropeDemo.context.drawingContext.moveTo(item.x, item.y+170);
-				ropeDemo.context.drawingContext.lineTo(3900, 1280);
-				ropeDemo.context.drawingContext.lineTo(-3900,1280);
-				ropeDemo.context.drawingContext.lineTo(item.x, item.y+170);
-				var grd = ropeDemo.context.drawingContext.createRadialGradient(0, 0, 2, 640, 1030, 1280);
-				
-				grd.addColorStop(0, 'white');
-				  // dark blue
-				grd.addColorStop(1, 'rgba(255, 255, 255, 0.6)');
+	console.log('LIGHT ON!');
 
-				// ropeDemo.context.drawingContext.fillStyle = 'rgba(255, 255, 209, 0.9)';
-				ropeDemo.context.drawingContext.fillStyle = grd;
-				ropeDemo.context.drawingContext.fill();
+	$('.glow').css('opacity',1);
 
-				ropeDemo.context.drawingContext.drawImage(ropeDemo.context.img1,-150,-10); 
-				
+	ropeDemo.data.state = true;
+	ropeDemo.context.drawingContext.beginPath();
+		
+	var item = ropeDemo.rope.items.slice(-1)[0];
+	var item0 = ropeDemo.rope.items.slice(-9)[0];
+	ropeDemo.rope.coeff = ((item.x + ropeDemo.context.center.x)- (item0.x + ropeDemo.context.center.x))/((item.y + ropeDemo.context.center.y)-(item0.y + ropeDemo.context.center.y));
+	ropeDemo.context.drawingContext.save();
+	ropeDemo.context.drawingContext.translate((item.x + ropeDemo.context.center.x), (item.y + ropeDemo.context.center.y));
+	ropeDemo.context.drawingContext.rotate(ropeDemo.rope.coeff);
+	
+//	//the light cone
+//	
+//	ropeDemo.context.drawingContext.moveTo(item.x, item.y+170);
+//	ropeDemo.context.drawingContext.lineTo(3900, 1280);
+//	ropeDemo.context.drawingContext.lineTo(-3900,1280);
+//	ropeDemo.context.drawingContext.lineTo(item.x, item.y+170);
+//	var grd = ropeDemo.context.drawingContext.createRadialGradient(0, 0, 2, 640, 1030, 1280);
+//	
+//	grd.addColorStop(0, 'white');
+//		// dark blue
+//	grd.addColorStop(1, 'rgba(255, 255, 255, 0.6)');
+//
+//	// ropeDemo.context.drawingContext.fillStyle = 'rgba(255, 255, 209, 0.9)';
+//	ropeDemo.context.drawingContext.fillStyle = grd;
+//	ropeDemo.context.drawingContext.fill();
 
-				ropeDemo.context.drawingContext.restore();
-				ropeDemo.context.drawingContext.lineWidth = 0;
-				ropeDemo.context.drawingContext.closePath();
-				$('.central2').css({'display':'block'});
+	ropeDemo.context.drawingContext.drawImage(ropeDemo.context.img1,ropeDemo.bulb.offsetX,ropeDemo.bulb.offsetY,ropeDemo.bulb.width,ropeDemo.bulb.height); 
+	
+
+	ropeDemo.context.drawingContext.restore();
+	ropeDemo.context.drawingContext.lineWidth = 0;
+	ropeDemo.context.drawingContext.closePath();
 }
 
 ropeDemo.noLight = function(){
 
-				ropeDemo.data.state = false;
-				$('.central').css({'opacity':'0'});
+	ropeDemo.data.state = false;
 
 
-				ropeDemo.context.drawingContext.clearRect(0, 60, ropeDemo.context.size.w, ropeDemo.context.size.h);
+	ropeDemo.context.drawingContext.clearRect(0, 60, ropeDemo.context.size.w, ropeDemo.context.size.h);
 
-				ropeDemo.context.drawingContext.beginPath();
-				var item = ropeDemo.rope.items.slice(-1)[0];
-				var item0 = ropeDemo.rope.items.slice(-9)[0];
-				ropeDemo.rope.coeff = ((item.x + ropeDemo.context.center.x)- (item0.x + ropeDemo.context.center.x))/((item.y + ropeDemo.context.center.y)-(item0.y + ropeDemo.context.center.y));
-				ropeDemo.context.drawingContext.save();
-				ropeDemo.context.drawingContext.translate((item.x + ropeDemo.context.center.x), (item.y + ropeDemo.context.center.y));
-				ropeDemo.context.drawingContext.rotate(ropeDemo.rope.coeff);
-							
-				ropeDemo.context.drawingContext.drawImage(ropeDemo.context.img0,-150,-10); 
+	ropeDemo.context.drawingContext.beginPath();
+	var item = ropeDemo.rope.items.slice(-1)[0];
+	var item0 = ropeDemo.rope.items.slice(-9)[0];
+	ropeDemo.rope.coeff = ((item.x + ropeDemo.context.center.x)- (item0.x + ropeDemo.context.center.x))/((item.y + ropeDemo.context.center.y)-(item0.y + ropeDemo.context.center.y));
+	ropeDemo.context.drawingContext.save();
+	ropeDemo.context.drawingContext.translate((item.x + ropeDemo.context.center.x), (item.y + ropeDemo.context.center.y));
+	ropeDemo.context.drawingContext.rotate(ropeDemo.rope.coeff);
 				
+	ropeDemo.context.drawingContext.drawImage(ropeDemo.context.img0,ropeDemo.bulb.offsetX,ropeDemo.bulb.offsetY,ropeDemo.bulb.width,ropeDemo.bulb.height); 
+	
 
-				ropeDemo.context.drawingContext.restore();
-				ropeDemo.context.drawingContext.lineWidth = 0;
-				ropeDemo.context.drawingContext.closePath();
-				$('.central2').css({'display':'none'});
-
+	ropeDemo.context.drawingContext.restore();
+	ropeDemo.context.drawingContext.lineWidth = 0;
+	ropeDemo.context.drawingContext.closePath();
 
 }
 ropeDemo.turnOff = function(){
-			// document.body.style.background="#FFF";
-			$('.dustCloser').css({'opacity':'0'}).removeClass('dustCloserAnimated');
-			$('.dustFar').css({'opacity':'0'}).removeClass('dustFarAnimated');
-
-			ropeDemo.noLight();
+	ropeDemo.noLight();
 }
 
 ropeDemo.StartOverride = function () {
 
 	ropeDemo.rope.items = [];
 	var i;
+	var length =  ropeDemo.rope.length / ropeDemo.rope.nbItems;
+	// rope
 	for (i = 0; i < ropeDemo.rope.nbItems; i++) {
-		var x =  ropeDemo.rope.length / ropeDemo.rope.nbItems;
+		var x =  length + i;
 		ropeDemo.rope.items[i] = {
 			x: x+0.1,
 			y: +0,
-			segmentLength: x,
+			segmentLength: length,
 			isRope: true,
 			old_x: x,
 			old_y: +0,
@@ -253,16 +261,16 @@ ropeDemo.StartOverride = function () {
 
 	}
 	i = ropeDemo.rope.nbItems-1;
-	var x = ropeDemo.rope.length / ropeDemo.rope.nbItems;
-		ropeDemo.rope.items[i] = {
-			x: x+0.1,
-			y: +0,
-			segmentLength: x,
-			isRope: false,
-			old_x: x,
-			old_y: +0,
-			isPinned: false
-		};
+	// bulb
+	ropeDemo.rope.items[i] = {
+		x: x+0.1,
+		y: +0,
+		segmentLength: length,
+		isRope: false,
+		old_x: x,
+		old_y: +0,
+		isPinned: false
+	};
 
 
 
